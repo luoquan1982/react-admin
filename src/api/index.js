a59d838385a42c8cced1555adb83744a87f41081
luoquan <luo.quan1982@gmail.com>
2019-06-10 20:47:51 +0800

    修改ajax引入问题bug

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@
  * 包含应用中所有接口请求函数的模块
  * 每个函数的返回值都是promise
  */
-import ajax from 'ajax';
+import ajax from './ajax';
 
 export const reqLogin = (username, password) => ajax('/login', {username, password}, 'POST');
 
