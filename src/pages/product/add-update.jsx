1abb080637539376671f9a5d7ce6f79a5fa072c0
luoquan <luo.quan1982@gmail.com>
2019-07-09 22:13:19 +0800

    完成商品修改的回显

--- src/pages/product/add-update.jsx
+++ src/pages/product/add-update.jsx
@@ -35,12 +35,30 @@ class ProductAddUpdate extends Component {
         }
     }
 
-    updateOptions = (categories) => {
+    updateOptions = async (categories) => {
         const options = categories.map((item) => ({
             value: item.id,
             label: item.name,
             isLeaf: false
         }))
+
+        //如果是一个二级分类商品的更新
+        const {isUpdate, product} = this;
+        const {paCategoryId, categoryId} = product;
+        if (isUpdate && '0' !== paCategoryId) {
+            //获取对应的二级分类列表
+            const subCategories = await this.getCategories(paCategoryId);
+            //生成二级分类列表
+            const subOptions = subCategories.map((item) => ({
+                value: item.id,
+                label: item.name,
+                isLeaf: true
+            }));
+            //找到当前商品对应的一级option对象
+            const targetOption = options.find(option=>option.value === paCategoryId)
+            //关联到对应的一级option上
+            targetOption.children = subOptions;
+        }
         this.setState({options});
     }
 
@@ -66,14 +84,15 @@ class ProductAddUpdate extends Component {
             //当前选中的分类没有二级分类,此时将标志位isLeaf的字段置了true
             targetOption.isLeaf = true;
         }
-        this.setState({options:[...this.state.options]})
+        this.setState({options: [...this.state.options]})
     };
 
     submit = () => {
         //进行表单验证,如果通过了,才发送请求
         this.props.form.validateFields((error, values) => {
             if (!error) {
-                alert('发送ajax请求')
+                console.log(values);
+                alert('发送ajax请求');
             }
         })
     }
@@ -89,11 +108,32 @@ class ProductAddUpdate extends Component {
         }
     }
 
+    componentWillMount() {
+        const product = this.props.history.location.state;
+        //是否是更新的标识符,如果不是,则为新增
+        this.isUpdate = !!product;
+        this.product = product || {};
+    }
+
     componentDidMount() {
         this.getCategories('0');
     }
 
     render() {
+        const {isUpdate, product} = this;
+        const {paCategoryId, categoryId} = product;
+        //级联分类的初始化数组,更新状态下初始化级联分类信息
+        const categoryIds = [];
+
+        if (isUpdate) {
+            //商品是一级分类的商品
+            if ('0' === paCategoryId) {
+                categoryIds.push(categoryId);
+            } else {
+                //商品是二级分类的商品
+                categoryIds.push(paCategoryId, categoryId);
+            }
+        }
 
         //指定Item布局的配置对象(antd栅格系统一共是24格)
         const formItemLayout = {
@@ -103,10 +143,10 @@ class ProductAddUpdate extends Component {
 
         const title = (
             <span>
-                <LinkButton>
+                <LinkButton onClick={() => this.props.history.goBack()}>
                     <Icon type="arrow-left" style={{fontSize: 20}}/>
                 </LinkButton>
-                <span>添加商品</span>
+                <span>{isUpdate ? '修改商品' : '添加商品'}</span>
             </span>
         );
 
@@ -116,7 +156,7 @@ class ProductAddUpdate extends Component {
                 <Form {...formItemLayout}>
                     <Item label='商品名称'>
                         {getFieldDecorator('name', {
-                            initialValue: '',
+                            initialValue: product.name,
                             rules: [
                                 {required: true, message: '商品名称必须填写'}
                             ]
@@ -124,7 +164,7 @@ class ProductAddUpdate extends Component {
                     </Item>
                     <Item label='商品描述'>
                         {getFieldDecorator('desc', {
-                            initialValue: '',
+                            initialValue: product.desc,
                             rules: [
                                 {required: true, message: '商品描述必须填写'}
                             ]
@@ -132,7 +172,7 @@ class ProductAddUpdate extends Component {
                     </Item>
                     <Item label='商品价格'>
                         {getFieldDecorator('price', {
-                            initialValue: '',
+                            initialValue: product.price,
                             rules: [
                                 {required: true, message: '商品价格必须填写'},
                                 {validator: this.validatePrice}
@@ -140,10 +180,20 @@ class ProductAddUpdate extends Component {
                         })(<Input type='number' placeholder='商品价格' addonAfter='元'/>)}
                     </Item>
                     <Item label='商品分类'>
-                        <Cascader
-                            options={this.state.options}    //需要显示的列表数据
-                            loadData={this.loadData}        //当选择某个列表项,加载下一级列表项
-                        />
+                        {
+                            getFieldDecorator('categoryIds', {
+                                initialValue: categoryIds,
+                                rules: [
+                                    {required: true, message: '商品分类必须填写'}
+                                ]
+                            })(
+                                <Cascader
+                                    placeholder='请指定商品分类信息'
+                                    options={this.state.options}    //需要显示的列表数据
+                                    loadData={this.loadData}        //当选择某个列表项,加载下一级列表项
+                                />
+                            )
+                        }
                     </Item>
                     <Item label='商品图片'>
                         <div>商品图片</div>
